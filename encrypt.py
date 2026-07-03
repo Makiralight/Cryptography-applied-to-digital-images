40e97e3784a0b8b4e5404f6f93223f3f16106465
Darlys-Laza <dlazab@unal.edu.co>
2026-07-03 16:49:21 -0500

    Prototipo funcional de las 3 capas para 1 píxel arbitrario

--- encrypt.py
+++ encrypt.py
@@ -1,4 +1,5 @@
 
+# Convertir la clave textual en un valor numérico
 clave = input("Ingrese una clave: ")
 
 suma = 0
@@ -6,6 +7,27 @@ suma = 0
 for letra in clave:
     suma = suma + ord(letra)
 
-k = suma % 256
+k = suma
+
+# Píxel de prueba
+x = 120
+
+# Capa 1: función biyectiva
+y = (13 * x + 57) % 256
+
+# Capa 2: suma modular usando la clave
+z = (y + k) % 256
+
+#capa 3 Descifrado
+# quitar la clave
+y_recuperado = (z - k) % 256
+
+# aplicar la función inversa
+x_recuperado = (197 * (y_recuperado - 57)) % 256  # 197 es el inverso modular de 13 módulo 256 (13⁻¹ mod 256 = 197)
 
 print("Valor de k:", k)
+print("Valor original:", x)
+print("Después de la función biyectiva:", y)
+print("Valor cifrado:", z)
+print("y recuperado:", y_recuperado)
+print("x recuperado:", x_recuperado)
